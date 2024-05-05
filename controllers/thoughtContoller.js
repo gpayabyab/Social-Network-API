80906d7c3f37c84b61df589271999b1dafd3ae33
Gabrielle 🌿 <gabrielle@gabrieleslaptop.mynetworksettings.com>
2024-05-04 22:29:31 -0400

    updaed user and thought controller files

--- controllers/thoughtContoller.js
+++ controllers/thoughtContoller.js
@@ -1,1 +1,66 @@
 // create functions for creating thougt, getting all thoughts, getting one thought, updating thought, deleting thought, adding reaction, removing reaction
+const { Thought } = require("../models");
+
+module.exports = {
+  async createThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.create(req.body);
+      res.json(dbThoughtData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async getThoughts(req, res) {
+    try {
+      const thoughts = await Thought.find();
+      res.json(thoughts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async getOneThought(req, res) {
+    try {
+      const thought = await Thought.findOne({ _id: req.params.thoughtId })
+        .select("-__v")
+        .populate("user");
+
+      if (!thought) {
+        return res.status(404).json({ message: "Cannot get thought!" });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async updateThought(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!thought) {
+        return res.status(404).json({ message: "cannot update thought!" });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  async deleteThought(req, res) {
+    try {
+      const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
+
+      if (!thought) {
+        return res.status(404).json({ message: "No thought with that ID" });
+      }
+      res.json({ message: "Thought deleted!" });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
--- controllers/userController.js
+++ controllers/userController.js
@@ -23,7 +23,7 @@ module.exports = {
     try {
       const user = await User.findOne({ _id: req.params.userId })
         .select("-__v")
-        .populate("posts");
+        .populate("user");
 
       if (!user) {
         return res.status(404).json({ message: "No user with that ID" });
